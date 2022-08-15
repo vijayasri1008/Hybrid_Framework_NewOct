05d6c9c7948a05f84fab388393fb681b2bc3f1fa
vijayasri1008 <110971303+vijayasri1008@users.noreply.github.com>
2022-08-15 20:38:39 +0530

    Update firstTest.java

--- src/test/java/demo/firstTest.java
+++ src/test/java/demo/firstTest.java
@@ -3,7 +3,7 @@ package demo;
 public class firstTest {
 
 	public static void main(String[] args) {
-System.out.println("first test added in local");
+System.out.println("first test added in remote");
 	}
 
 }
