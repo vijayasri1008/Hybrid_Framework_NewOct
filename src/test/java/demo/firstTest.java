573206ce7947728c534f4a7434a999533411e773
vijayasri1008 <vijayasri1008@gmail.com>
2022-08-15 20:56:06 +0530

    test added

--- src/test/java/demo/firstTest.java
+++ src/test/java/demo/firstTest.java
@@ -3,7 +3,10 @@ package demo;
 public class firstTest {
 
 	public static void main(String[] args) {
-System.out.println("first test added in remote");
+				System.out.println("first test added in local");
+			
+		
+
 	}
 
 }
